ba08f70a4a28d66b4600f6564e331e31bb159275
edward mcenrue <edward.mcenrue@gmail.com>
2015-10-27 06:49:15 -0400

    migrations manager

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
+
+#print(os.environ['APP_SETTINGS'])
+print(os.environ['DATABASE_URL'])
+
 
 @app.route('/')
 def hello():
@@ -12,6 +20,8 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
 	return "Hello {}!".format(name)
+
+
 
 if __name__ == '__main__':
 	app.run()
